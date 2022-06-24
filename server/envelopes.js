4d71314473391c47c5772349ccbdca88266fc910
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-06-24 17:47:06 +0200

    transfer value

--- server/envelopes.js
+++ server/envelopes.js
@@ -68,6 +68,36 @@ envelopesRouter.delete('/:envelopeId', (req, res, next) => {
     
 })
 
+envelopesRouter.post('/transfer/:from/:to', (req, res, next) => {
+    try {
+        const {from, to} = req.params;
+        const {value} = req.body;
+        
+        const fromEnvleope = envelopes.find((e) => {
+            return e.id == from;
+        })
+        const toEnvelope = envelopes.find((e) => {
+            return e.id == to;
+        })
+        if (fromEnvleope && toEnvelope){
+            if (fromEnvleope.budget >= value){
+                fromEnvleope.budget -= value;
+                toEnvelope.budget += value;
+                res.sendStatus(200);
+            } else {
+                res.status(400).send({
+                    message: "Amount to transfer exceeds the amount in the envelope"
+                })
+            }
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+    
+})
+
 
 
 
