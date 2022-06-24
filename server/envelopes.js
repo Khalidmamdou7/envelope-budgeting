49eedce097590dbd6eb3aa639544126c76659516
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-06-24 17:12:55 +0200

    update envelope

--- server/envelopes.js
+++ server/envelopes.js
@@ -24,11 +24,32 @@ envelopesRouter.get('/:envelopeId', (req, res, next) => {
         return e.id == envelopeId;
     })
     if (envelope.length){
-        res.status(200).send(envelope);
+        res.status(200).send(envelope[0]);
     } else {
         res.sendStatus(404);
     }
 })
+
+envelopesRouter.put('/:envelopeId', (req, res, next) => {
+    try {
+        const {envelopeId} = req.params;
+        const envelope = envelopes.filter((e) => {
+            return e.id == envelopeId;
+        })
+        if (envelope.length){
+            const {title, budget} = req.body;
+            envelope[0].title = title;
+            envelope[0].budget = budget;
+            res.status(201).send(envelope[0]);
+        } else {
+            res.sendStatus(404);
+        }
+    } catch (err) {
+        res.status(500).send(err);
+    }
+    
+})
+
 
 
 
