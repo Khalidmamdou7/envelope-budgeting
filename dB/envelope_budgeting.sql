d7d74db67bc526db4a32bb9210b4f78745a4e034
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-07-11 04:34:59 +0200

    A representation for the database

---
+++ dB/envelope_budgeting.sql
@@ -0,0 +1,41 @@
+CREATE TABLE "users" (
+  "username" varchar PRIMARY KEY,
+  "full_name" varchar not null,
+  "password" varchar not null,
+  "created_at" timestamp
+);
+
+CREATE TABLE "envelopes" (
+  "id" SERIAL PRIMARY KEY,
+  "title" varchar not null,
+  "budget" int
+);
+
+CREATE TABLE "transactions" (
+  "id" SERIAL PRIMARY KEY,
+  "username" varchar not null,
+  "value" int not null,
+  "time" timestamp
+);
+
+CREATE TABLE "envelopes_users" (
+  "envelope_id" int,
+  "username" varchar,
+  PRIMARY Key(envelope_id, username)
+);
+
+CREATE TABLE "transactions_envelopes" (
+  "envelope_id" int,
+  "transaction_id" int,
+  PRIMARY Key(envelope_id, transaction_id)
+);
+
+ALTER TABLE "users" ADD FOREIGN KEY ("username") REFERENCES "transaction" ("username");
+
+ALTER TABLE "transaction_envelopes" ADD FOREIGN KEY ("transaction_id") REFERENCES "transaction" ("id");
+
+ALTER TABLE "transaction_envelopes" ADD FOREIGN KEY ("envelope_id") REFERENCES "envelopes" ("id");
+
+ALTER TABLE "envelopes_users" ADD FOREIGN KEY ("username") REFERENCES "users" ("username");
+
+ALTER TABLE "envelopes_users" ADD FOREIGN KEY ("envelope_id") REFERENCES "envelopes" ("id");
