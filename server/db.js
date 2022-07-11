0f9ac5aa91d8da3baa0ebcaaa969e9ec57d98aa0
Khaled Mamdouh <37771185+Khalidmamdou7@users.noreply.github.com>
2022-07-11 19:38:42 +0200

    Add and tested transactions endpoints

--- server/db.js
+++ server/db.js
@@ -85,6 +85,56 @@ const createTransaction = (req, res) => {
     )
 }
 
+const getTransactions = (req, res) => {
+    pool.query('SELECT * from transactions ORDER BY id ASC',
+     (error, results) => {
+        if (error){
+            throw error;
+        }
+        res.status(200).json(results.rows);
+    })
+}
+
+const getTransactionsByUserName = (req, res) => {
+    const username = req.params.username;
+    pool.query(
+        'SELECT * from transactions where username = $1 order by id asc', 
+        [username], 
+        (error, results) => {
+            if (error){
+                throw error;
+            }
+            res.status(200).json(results.rows);
+        }
+    )
+}
+
+const updateTransaction = (req, res) => {
+    const id = parseInt(req.params.transactionId);
+    const {username, value, envelopeId} = req.body;
+    pool.query(
+        'UPDATE transactions set username = $1, value = $2, envelope_id = $3 where id = $4',
+        [username, value, envelopeId, id],
+        (error, results) => {
+            console.log(results.rows);
+            if (error) {
+                throw error;
+            }
+            res.status(201).send(`Transaction modified with id: ${id}`);
+        }
+    )
+}
+
+const deleteTransaction = (req, res) => {
+    const id = parseInt(req.params.transactionId);
+    pool.query('DELETE FROM transactions WHERE id = $1', [id], (error, results) => {
+        if (error) {
+          throw error;
+        }
+        res.status(200).send(`transaction deleted with ID: ${id}`);
+      })
+}
+
 module.exports = {
     getEnvelopes,
     createEnvelope,
@@ -92,5 +142,9 @@ module.exports = {
     updateEnvelope,
     deleteEnvelope, 
     createTransaction,
+    getTransactions, 
+    getTransactionsByUserName,
+    updateTransaction,
+    deleteTransaction
 };
 
--- server/routers/transactions.js
+++ server/routers/transactions.js
@@ -2,5 +2,9 @@ const transactionsRouter = require('express').Router();
 const db = require('../db');
 
 transactionsRouter.post('/', db.createTransaction);
+transactionsRouter.get('/', db.getTransactions);
+transactionsRouter.put('/:transactionId', db.updateTransaction);
+transactionsRouter.get('/:username', db.getTransactionsByUserName);
+transactionsRouter.delete('/:transactionId', db.deleteTransaction);
 
 module.exports = transactionsRouter;
